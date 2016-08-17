c37ce53cc3ed96ad022328c4d878f03fcba07439
Lena Ngungu <lenangungu@hotmail.co.uk>
2016-08-17 17:21:49 -0500

    first and second letter row display
    only the first row is connected and can carry action(type on the
    textView)

---
+++ VodaCustomKeyboard2/KeyboardViewController.swift
@@ -0,0 +1,148 @@
+//
+//  KeyboardViewController.swift
+//  VodaCustomKeyboard2
+//
+//  Created by Lena Ngungu on 8/17/16.
+//  Copyright © 2016 Lena Ngungu Corp. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        let nib = UINib(nibName: "VodaKeyboardView2", bundle: nil)
+        let objects = nib.instantiateWithOwner(self, options: nil)
+        view = objects[0] as! UIView;
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .System)
+    
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+    
+        self.nextKeyboardButton.addTarget(self, action: #selector(advanceToNextInputMode), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+    
+        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+        
+        
+        
+        
+        // Adding buttons to keyboard
+        let buttonsTitle = ["Q","W","E","R","T","Y"]
+        var buttons = createButtons(buttonsTitle)
+        var topRow = UIView(frame: CGRectMake(0,0,320,40))
+        
+        for button in buttons
+        {
+            topRow.addSubview(button)
+        }
+        view.addSubview(topRow)
+        addConstraints(buttons, containingView: topRow)
+        
+    }
+    
+    func createButtons(titles: [String]) -> [UIButton]
+    {
+        var buttons = [UIButton]()
+        for title in titles{
+            
+            let button = UIButton(type: .System)
+            
+            button.setTitle(title, forState: .Normal)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.backgroundColor = UIColor(white: 1.0, alpha: 1.0)
+            button.setTitleColor(UIColor.darkGrayColor(), forState: .Normal)
+            button.addTarget(self, action: #selector(KeyboardViewController.keyPressed(_:)), forControlEvents: .TouchUpInside)
+            buttons.append(button)
+        }
+        return buttons
+    }
+    func keyPressed(sender: AnyObject?)
+    {
+        let button = sender as! UIButton
+        let title = button.titleForState(.Normal)
+        (textDocumentProxy as UIKeyInput).insertText(title!)
+    }
+    
+    func addConstraints(buttons: [UIButton], containingView: UIView)
+    {
+        //for (index, button) in
+        
+        for (index, button) in  buttons.enumerate()
+        {
+            
+            var topConstraint = NSLayoutConstraint(item: button, attribute: .Top, relatedBy: .Equal, toItem: containingView, attribute: .Top, multiplier: 1.0, constant: 1)
+            
+            var bottomConstraint = NSLayoutConstraint(item: button, attribute: .Bottom, relatedBy: .Equal, toItem: containingView, attribute: .Bottom, multiplier: 1.0, constant: -1)
+            
+            var leftConstraint : NSLayoutConstraint!
+            
+            if index == 0 {
+                
+                leftConstraint = NSLayoutConstraint(item: button, attribute: .Left, relatedBy: .Equal, toItem: containingView, attribute: .Left, multiplier: 1.0, constant: 1)
+                
+            }else{
+                
+                leftConstraint = NSLayoutConstraint(item: button, attribute: .Left, relatedBy: .Equal, toItem: buttons[index-1], attribute: .Right, multiplier: 1.0, constant: 1)
+                
+                var widthConstraint = NSLayoutConstraint(item: buttons[0], attribute: .Width, relatedBy: .Equal, toItem: button, attribute: .Width, multiplier: 1.0, constant: 0)
+                
+                containingView.addConstraint(widthConstraint)
+            }
+            
+            var rightConstraint : NSLayoutConstraint!
+            
+            if index == buttons.count - 1 {
+                
+                rightConstraint = NSLayoutConstraint(item: button, attribute: .Right, relatedBy: .Equal, toItem: containingView, attribute: .Right, multiplier: 1.0, constant: -1)
+                
+            }else{
+                
+                rightConstraint = NSLayoutConstraint(item: button, attribute: .Right, relatedBy: .Equal, toItem: buttons[index+1], attribute: .Left, multiplier: 1.0, constant: -1)
+            }
+            
+            containingView.addConstraints([topConstraint, bottomConstraint, rightConstraint, leftConstraint])
+        }
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+
+}
---
+++ VodaKeyboard2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VodaKeyboard2
+//
+//  Created by Lena Ngungu on 8/17/16.
+//  Copyright © 2016 Lena Ngungu Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
